d7073a8b26a48fb08aa2845b4a1d3315395f7d9a
jflo02 <florimond2002@gmail.com>
2021-01-19 00:06:15 +0100

    refonte du placement des pieces + début du mouevement

--- chess.py
+++ chess.py
@@ -11,10 +11,58 @@ class Plateau:
     def __init__(self):
         self.tableau = []
         self.create()
-        self.placer_pieces()  
+        
         self.dicoLettreToPos = { "A" : 0, "B" : 1, "C" : 2, "D" : 3, "E" : 4, "F" : 5, "G" : 6, "H" : 7 }
         self.dicoChiffreToPos = { "8" : 0, "7" : 1, "6" : 2, "5" : 3, "4" : 4, "3" : 5, "2" : 6, "1" : 7}
-        self.dicoLettreToPiece = {"P" : "ton Pion", "T" : "ta Tour", "C" : "ton Cavalier", "D" : "ta Dame", "R" : "ton Roi", "F" : "ton Fou"}
+        
+        self.pionBlanc1 = PionBlanc(0, 1)
+        self.pionBlanc2 = PionBlanc(1, 1)
+        self.pionBlanc3 = PionBlanc(2, 1)
+        self.pionBlanc4 = PionBlanc(3, 1)
+        self.pionBlanc5 = PionBlanc(4, 1)
+        self.pionBlanc6 = PionBlanc(5, 1)
+        self.pionBlanc7 = PionBlanc(6, 1)
+        self.pionBlanc8 = PionBlanc(7, 1)
+
+        self.tourBlanc1 = TourBlanc(0, 0)
+        self.tourBlanc2 = TourBlanc(7, 0)
+        
+        self.cavalierBlanc1 = CavalierBlanc(1, 0)
+        self.cavalierBlanc2 = CavalierBlanc(6, 0)
+        
+        self.fouBlanc1 = FouBlanc(2, 0)
+        self.fouBlanc2 = FouBlanc(5, 0)
+
+        self.roiBlanc = RoiBlanc(3, 0)
+
+        self.dameBlanc = DameBlanc(4, 0)
+        
+        self.pionNoir1 = PionNoir(0, 6)
+        self.pionNoir2 = PionNoir(1, 6)
+        self.pionNoir3 = PionNoir(2, 6)
+        self.pionNoir4 = PionNoir(3, 6)
+        self.pionNoir5 = PionNoir(4, 6)
+        self.pionNoir6 = PionNoir(5, 6)
+        self.pionNoir7 = PionNoir(6, 6)
+        self.pionNoir8 = PionNoir(7, 6)
+        
+        self.tourNoir1 = TourNoir(0, 7)
+        self.tourNoir2 = TourNoir(7, 7)
+        
+        self.cavalierNoir1 = CavalierNoir(1, 7)
+        self.cavalierNoir2 = CavalierNoir(6, 7)
+
+        self.fouNoir1 = FouNoir(2, 7)
+        self.fouNoir2 = FouNoir(5, 7)
+        
+        self.roiNoir = RoiNoir(3, 7)
+        
+        self.dameNoir = DameNoir(4, 7)
+        
+         
+        self.tabDesPieces = [self.pionBlanc1, self.pionBlanc2,self.pionBlanc3, self.pionBlanc4, self.pionBlanc5, self.pionBlanc6, self.pionBlanc7, self.pionBlanc8, self.tourBlanc1, self.tourBlanc2, self.cavalierBlanc1, self.cavalierBlanc2, self.fouBlanc1, self.fouBlanc2, self.roiBlanc, self.dameBlanc, self.pionNoir1, self.pionNoir2,self.pionNoir3, self.pionNoir4, self.pionNoir5, self.pionNoir6, self.pionNoir7, self.pionNoir8, self.tourNoir1, self.tourNoir2, self.cavalierNoir1, self.cavalierNoir2, self.fouNoir1, self.fouNoir2, self.roiNoir, self.dameNoir]
+
+        self.maj_pieces()
 
     def create(self):
        
@@ -34,52 +82,39 @@ class Plateau:
         repr = repr + '  ' + '—'+ '—'+ '—'+ '—'+ '—'+ '—'+ '—' + '—'+ '—'+ '—' + '—'+ '—' + '—'+ '—'+ '—' + '—'+ '—'
         print(repr)#à la fin on affiche la représentation globale du tableau, avec toutes les lignes
 
-    def placer_pieces(self):
-        pionBlanc = PionBlanc()
-        pionNoir = PionNoir()
-        tourBlanc = TourBlanc()
-        tourNoir = TourNoir()
-        cavalierBlanc = CavalierBlanc()
-        cavalierNoir = CavalierNoir()
-        fouNoir = FouNoir()
-        fouBlanc = FouBlanc()
-        roiNoir = RoiNoir()
-        roiBlanc = RoiBlanc()
-        dameNoir = DameNoir()
-        dameBlanc = DameBlanc()
-        #on place les pions
-        for i in range (0,8):
-            self.tableau[1][i] = pionNoir.get_affichage()
-            self.tableau[6][i] = pionBlanc.get_affichage()
-        #on place les tours
-        self.tableau[0][0] = self.tableau[0][7] = tourNoir.get_affichage()
-        self.tableau[7][0] = self.tableau[7][7] = tourBlanc.get_affichage()
-        #puis ainsi de suite
-        self.tableau[0][1] = self.tableau[0][6] = cavalierNoir.get_affichage()
-        self.tableau[7][1] = self.tableau[7][6] = cavalierBlanc.get_affichage()
+    def maj_pieces(self):
+        self.tableau = []
+        for i in range (8):
+            self.tableau.append([" ", " ", " ", " ", " ", " ", " ", " "])
+        
+        for piece in self.tabDesPieces:
+            self.tableau[piece.coordY][piece.coordX] = piece.affichage
 
-        self.tableau[0][2] = self.tableau[0][5] = fouNoir.get_affichage()
-        self.tableau[7][2] = self.tableau[7][5] = fouBlanc.get_affichage()
-
-        self.tableau[0][3] = roiNoir.get_affichage()
-        self.tableau[7][3] = roiBlanc.get_affichage()
-
-        self.tableau[0][4] = dameNoir.get_affichage()
-        self.tableau[7][4] = dameBlanc.get_affichage()
 
     def ask_coup(self):
         print("quelles sont les coordonnées du pion que tu veux jouer ?")
         pionAbouger = input()
         lettre = self.dicoLettreToPos[pionAbouger[0].upper()]
         chiffre = self.dicoChiffreToPos[pionAbouger[1]]
-        pionSurCase = self.tableau[chiffre][lettre]
-        print(f"Ou veux-tu déplacer {self.dicoLettreToPiece[pionSurCase.upper()]} ? (exemple A5)")
+        pionSurCase = self.quelPionSurCetteCase(lettre, chiffre)
+        
+        print(f"Ou veux-tu déplacer {pionSurCase.nom} ? (exemple A5)")
+        
         caseVoulu = input()
-        lettreVoulu = self.dicoLettreToPos[caseVoulu[0].upper()]
-        chiffreVoulu = self.dicoChiffreToPos[caseVoulu[1]]
+        pionSurCase.coordX = self.dicoLettreToPos[caseVoulu[0].upper()]
+        pionSurCase.coordY = self.dicoChiffreToPos[caseVoulu[1]]
+
+    def quelPionSurCetteCase(self, x, y):
+        for pieces in self.tabDesPieces:
+            if pieces.coordX == x and pieces.coordY == y:
+                break
+        return pieces #on return l'indoce de l'objet qu'on souhaite
 
 
 
 plateau = Plateau()
 plateau.afficher()
-plateau.ask_coup()
+for i in range (4):
+    plateau.ask_coup()
+    plateau.maj_pieces()
+    plateau.afficher()
--- pieces.py
+++ pieces.py
@@ -1,6 +1,8 @@
 class Piece :
-    def __init__(self):
-        self.nom = "piece"
+    def __init__(self, coordX, coordY):
+        self.nom = "pièce"
+        self.coordX = coordX
+        self.coordY = coordY
 
     def get_affichage(self):
         return self.affichage
@@ -8,62 +10,86 @@ class Piece :
 
 class PionBlanc(Piece):
     
-    def __init__(self):
+    def __init__(self, coordX, coordY):
+        super().__init__(coordX, coordY)
+        self.nom = "Pion Blanc"
         self.affichage = "P"
 
     
 
 class PionNoir(Piece):
 
-    def __init__(self):
+    def __init__(self, coordX, coordY):
+        super().__init__(coordX, coordY)
+        self.nom = "Pion Noir"
         self.affichage = "p"
 
 class TourBlanc(Piece):
 
-    def __init__(self):
+    def __init__(self, coordX, coordY):
+        super().__init__(coordX, coordY)
+        self.nom = "Tour Blanche"
         self.affichage = "T"
 
 class TourNoir(Piece):
 
-    def __init__(self):
+    def __init__(self, coordX, coordY):
+        super().__init__(coordX, coordY)
+        self.nom = "Tour Noire"
         self.affichage = "t"
 
 class CavalierBlanc(Piece):
 
-    def __init__(self):
+    def __init__(self, coordX, coordY):
+        super().__init__(coordX, coordY)
+        self.nom = "Cavalier Blanc"
         self.affichage = "C"
 
 class CavalierNoir(Piece):
 
-    def __init__(self):
+    def __init__(self, coordX, coordY):
+        super().__init__(coordX, coordY)
+        self.nom = "Cavalier Noir"
         self.affichage = "c"
 
 class FouBlanc(Piece):
 
-    def __init__(self):
+    def __init__(self, coordX, coordY):
+        super().__init__(coordX, coordY)
+        self.nom = "Fou Blanc"
         self.affichage = "F"
 
 class FouNoir(Piece):
 
-    def __init__(self):
+    def __init__(self, coordX, coordY):
+        super().__init__(coordX, coordY)
         self.affichage = "f" 
 
 class RoiBlanc(Piece):
 
-    def __init__(self):
+    def __init__(self, coordX, coordY):
+        super().__init__(coordX, coordY)
         self.affichage = "R"
 
 class RoiNoir(Piece):
 
-    def __init__(self):
+    def __init__(self, coordX, coordY):
+        super().__init__(coordX, coordY)
         self.affichage = "r"
 
 class DameBlanc(Piece):
 
-    def __init__(self):
+    def __init__(self, coordX, coordY):
+        super().__init__(coordX, coordY)
         self.affichage = "D"
 
 class DameNoir(Piece):
 
-    def __init__(self):
+    def __init__(self, coordX, coordY):
+        super().__init__(coordX, coordY)
         self.affichage = "d" 
+
+
+testpion = PionBlanc(0, 1)
+
+print(testpion.nom)
